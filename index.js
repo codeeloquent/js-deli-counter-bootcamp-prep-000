083650f626ff9c3b0c606ec6bb5ccb93dbcd0a1c
Ryan Eckerle <e_ryan1@yahoo.com>
2017-12-13 21:52:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,11 +18,18 @@ function currentLine(katzDeliLine){
   if(katzDeliLine.length > 0){
     var array = [];
     let num = 1;
+<<<<<<< HEAD
     for(let i = 0; i < katzDeliLine.length; i++){
       array.push(`${num}. ${katzDeliLine[i]}`);
       num++;
     }
     return "The line is currently: " + array.join(', ');
+=======
+    for(let i = 0; i < katzDeliLine.length -1 ; i++){
+      array.push(`${num}. ${katzDeliLine[i]}`);
+    }
+    return "The line is currently: " + array;
+>>>>>>> 1c40aa32abf84498739dfdb9bc61ee1db41a431d
   }
   else{
       return 'The line is currently empty.';
